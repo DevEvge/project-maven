c64678554281eee54bbe53b09cf9a0791e37eba5
DevEvge <evgeniivolkov18@gmail.com>
2025-01-26 12:44:35 +0200

    fix

--- bin/maven-install.bash
+++ bin/maven-install.bash
@@ -1,2 +1,2 @@
 
-mvn install:install-file -Dfile=lib/desktop-game-engine.jar -DgroupId=com.javarush -DartifactId=desktop-game-engine -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=C:\Users\evgen\IdeaProjects\project-maven\bin\maven-install.bash -DgroupId=com.javarush -DartifactId=desktop-game-engine -Dversion=1.0 -Dpackaging=jar
